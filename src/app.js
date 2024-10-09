c76d569782f5febf68f7b4ef509acbab3267dc94
ViesK <109549664+ViesK@users.noreply.github.com>
2024-10-09 10:58:25 +0000

    feat: nuevo proyecto

--- src/app.js
+++ src/app.js
@@ -6,6 +6,36 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //Objeto que recoge todos combinaciones del generador de excusas agrupadas en arrays.
+  var excuses = {
+    who: ["The dog", "My grandma", "The mailman", "My bird"],
+    action: ["ate", "peed", "crushed", "broke"],
+    what: ["my homework", "my phone", "the car"],
+    when: [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying"
+    ]
+  };
+
+  //Le llega un array de elementos y devuelve un elemento del array usando la función aleatoria.
+  const randomExcuseEl = arr => arr[Math.floor(Math.random() * arr.length)];
+
+  // Recorre el objeto "excuse" del primero al último elemento y en cada uno llama a la funcion "randomExcuseEl"
+  // Luego almacena el valor devuelto en una variable "excuseFrase" para devolverla como respuesta final del ejercicio.
+  function excusesGenerator() {
+    let excuseFrase = "";
+
+    for (let key in excuses) {
+      let randomEl = randomExcuseEl(excuses[key]);
+      excuseFrase += `${randomEl} `;
+    }
+    return excuseFrase;
+  }
+
+  console.log(excusesGenerator());
+
+  document.getElementById("excuse").textContent = excusesGenerator();
 };
